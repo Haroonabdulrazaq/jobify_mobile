063d62855639057db136f28504e8d322ef0782ba
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2023-04-15 08:31:10 +0100

    Add job details to the details page

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -7,9 +7,125 @@ import {
   ScreenHeaderBtn,
   Specifics,
 } from "../../components";
+import {
+  Text,
+  SafeAreaView,
+  ScrollView,
+  RefreshControl,
+  ActivityIndicator,
+  View,
+} from "react-native";
+import { COLORS, icons, SIZES } from "../../constants";
+import useFetch from "../../hook/useFetch";
+import { Stack, useRouter, useSearchParams } from "expo-router";
+
+const tabs = ["About", "Qualifications", "Responsibilities"];
 
 const JobDetails = () => {
-  return <div>JobDetails</div>;
+  const params = useSearchParams();
+  const router = useRouter();
+  const [refreshing, setRefreshing] = useState(false);
+  const [activeTab, setActiveTab] = useState(tabs[0]);
+  const onRefresh = () => {};
+
+  const displayTabContent = () => {
+    switch (activeTab) {
+      case "About":
+        return (
+          <JobAbout
+            info={data[0].job_description ?? ["N/A"]}
+            benefits={data[0]?.job_benefits}
+          />
+        );
+        break;
+      case "Qualifications":
+        return (
+          <Specifics
+            title="Qualifications"
+            points={data[0].job_highlights?.Qualifications ?? ["N/A"]}
+          />
+        );
+        break;
+      case "Responsibilities":
+        return (
+          <Specifics
+            title="Responsibilities"
+            points={data[0].job_highlights.Responsibilities ?? ["N/A"]}
+          />
+        );
+        break;
+
+      default:
+        break;
+    }
+  };
+  const { data, error, isLoading, refetch } = useFetch("job-details", {
+    job_id: params.id,
+  });
+
+  // console.log(params);
+  // console.log(data);
+  // console.log(error);
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: COLORS.lightWhite },
+          headerShadowVisible: false,
+          headerBackVisible: false,
+          headerLeft: () => (
+            <ScreenHeaderBtn
+              iconUrl={icons.left}
+              dimension="60%"
+              handlePress={() => router.back()}
+            />
+          ),
+          headerRight: () => (
+            <ScreenHeaderBtn iconUrl={icons.share} dimension="60%" />
+          ),
+          headerTitle: "",
+        }}
+      />
+      <>
+        <ScrollView
+          showVerticalIndicator={false}
+          refreshControl={
+            <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+          }
+        >
+          {isLoading ? (
+            <ActivityIndicator size="large" color={COLORS.primary} />
+          ) : error ? (
+            <Text>Something went wrong</Text>
+          ) : data.length === 0 ? (
+            <Text>No data</Text>
+          ) : (
+            <View style={{ padding: SIZES.medium, paddingBottom: 100 }}>
+              <Company
+                companyLogo={data[0].employer_logo}
+                jobTitle={data[0].job_title}
+                companyName={data[0].employer_name}
+                location={data[0].job_country}
+              />
+              <JobTabs
+                tabs={tabs}
+                activeTab={activeTab}
+                setActiveTab={setActiveTab}
+              />
+              {displayTabContent()}
+              <JobFooter
+                url={
+                  data[0]?.job_google_link ??
+                  data[0]?.job_apply_link ??
+                  "https://careers.google.com/jobs/results"
+                }
+              />
+            </View>
+          )}
+        </ScrollView>
+      </>
+    </SafeAreaView>
+  );
 };
 
 export default JobDetails;
--- components/jobdetails/about/About.jsx
+++ components/jobdetails/about/About.jsx
@@ -2,15 +2,26 @@ import { View, Text } from "react-native";
 
 import styles from "./about.style";
 
-const About = ({ info }) => {
+const About = ({ info, benefits }) => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.headText}>About the job:</Text>
+    <>
+      <View style={styles.container}>
+        <Text style={styles.headText}>About the job:</Text>
 
-      <View style={styles.contentBox}>
-        <Text style={styles.contextText}>{info}</Text>
+        <View style={styles.contentBox}>
+          <Text style={styles.contextText}>{info}</Text>
+        </View>
       </View>
-    </View>
+      <View style={styles.container}>
+        <Text style={styles.headText}>Benefits</Text>
+        {benefits.map((item, index) => (
+          <View style={styles.pointWrapper} key={item + index}>
+            <View style={styles.pointDot} />
+            <Text style={styles.pointText}>{item}</Text>
+          </View>
+        ))}
+      </View>
+    </>
   );
 };
 
